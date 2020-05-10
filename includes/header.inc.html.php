2fe227e7d3a763cffa44d9c217901b0c3bdd0179
Dylan Dills <dylan.dills@gmail.com>
2020-05-09 18:33:57 -0700

    Navigation style template done

--- includes/header.inc.html.php
+++ includes/header.inc.html.php
@@ -3,12 +3,10 @@
   <a href="#home" class="logo">Logo</a>
   <input class="menu-btn" type="checkbox" id="menu-btn" />
   <label class="menu-icon" for="menu-btn"><span class="nav-icon"></span></label>
-  <nav class="menu">
-        <ul>
-          <li><a href="#home">Home</a></li>
-          <li><a href="#about">About</a></li>
-          <li><a href="#projects">Projects</a></li>
-          <li><a href="#contact">Contact</a></li>
-        </ul>
-  </nav>
+  <ul class="menu">
+    <li><a href="#home">Home</a></li>
+    <li><a href="#about">About</a></li>
+    <li><a href="#projects">Projects</a></li>
+    <li><a href="#contact">Contact</a></li>
+  </ul>
 </header>
