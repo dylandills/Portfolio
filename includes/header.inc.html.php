00d464b4ebb0ad011655fbafb2f07586e14995c2
Dylan Dills <dylan.dills@gmail.com>
2020-05-09 17:58:36 -0700

    changing directions and creating hamburger menu based off tutorial https://www.youtube.com/watch?v=sjrp1FEHnyA&t=1807s

--- includes/header.inc.html.php
+++ includes/header.inc.html.php
@@ -1,12 +1,14 @@
 <?php define('BASE_URL', '/cas285/portfolio/'); ?>
-<header>
-  <a href="#home" class="logo" data-scroll>Fixed Nav</a>
-  <nav class="nav-collapse">
-    <ul>
-      <li class="menu-item active"><a href="#home" data-scroll>Home</a></li>
-      <li class="menu-item"><a href="#about" data-scroll>About</a></li>
-      <li class="menu-item"><a href="#projects" data-scroll>Projects</a></li>
-      <li class="menu-item"><a href="#contact" data-scroll>Contact</a></li>
-    </ul>
+<header class="header">
+  <a href="#home" class="logo">Logo</a>
+  <input class="menu-btn" type="checkbox" id="menu-btn" />
+  <label class="menu-icon" for="menu-btn"><span class="nav-icon"></span></label>
+  <nav class="menu">
+        <ul>
+          <li><a href="#home">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#projects">Projects</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
   </nav>
 </header>
