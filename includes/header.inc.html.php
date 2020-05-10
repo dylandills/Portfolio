8c8b4facdf3c0a8422ef9c49074a93613e2cf9e2
Dylan Dills <dylan.dills@gmail.com>
2020-05-09 22:14:18 -0700

    Fixed header full width issue

--- includes/header.inc.html.php
+++ includes/header.inc.html.php
@@ -2,7 +2,9 @@
 <header class="header">
   <a href="#home" class="logo">Logo</a>
   <input class="menu-btn" type="checkbox" id="menu-btn" />
-  <label class="menu-icon" for="menu-btn"><span class="nav-icon"></span></label>
+  <label class="menu-icon" for="menu-btn">
+    <span class="nav-icon"></span>
+  </label>
   <ul class="menu">
     <li><a href="#home">Home</a></li>
     <li><a href="#about">About</a></li>
