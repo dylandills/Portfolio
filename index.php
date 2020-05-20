47f342a91c9cbe3b99f45a97fcc4ae4a05da702b
Dylan Dills <dylan.dills@gmail.com>
2020-05-19 18:48:57 -0700

    Set up the About Section with title, image, and paragraph styles

--- index.php
+++ index.php
@@ -8,83 +8,86 @@
       <section id="home">
         <div class="container">
           <div class="icon animate__animated animate__bounceInLeft">
-            <img src="img/book.png" alt="Book Icon" />
-          </div>
-          <div class="icon animate__animated animate__bounceInLeft">
-            <img src="img/chat.png" alt="Chat Icon" w/>
-          </div>
-          <div class="icon animate__animated animate__bounceInLeft">
-            <img src="img/headphones.png" alt="Headphones Icon" />
-          </div>
-          <div class="icon animate__animated animate__bounceInLeft">
-            <img src="img/magnifying-glass.png" alt="Magnifying Glass Icon" />
-          </div>
-          <div class="icon animate__animated animate__bounceInLeft">
-            <img src="img/envelope.png" alt="Envelope Icon" />
-          </div>
-          <div class="icon animate__animated animate__bounceInLeft">
-            <img src="img/share.png" alt="Share Icon" />
-          </div>
-          <div class="icon animate__animated animate__bounceInLeft">
-            <img src="img/speaker.png" alt="Speaker Icon" />
-          </div>
-          <div class="icon animate__animated animate__bounceInLeft">
-            <img src="img/upload.png" alt="Upload Icon" />
-          </div>
-          <div class="icon animate__animated animate__bounceInLeft">
-            <img src="img/padlock.png" alt="Padlock Icon" />
-          </div>
-          <div class="icon animate__animated animate__bounceInLeft">
-            <img src="img/back.png" alt="Back Icon" />
-          </div>
-          <div class="icon animate__animated animate__bounceInLeft">
-            <img src="img/next-1.png" alt="Next Icon" />
-          </div>
-          <div class="icon animate__animated animate__bounceInLeft">
-            <img src="img/monitor.png" alt="Monitor Icon" />
-          </div>
-          <div class="icon animate__animated animate__bounceInLeft">
-            <img src="img/reload.png" alt="Reload Icon" />
-          </div>
-          <div class="icon animate__animated animate__bounceInLeft">
-            <img src="img/musical-note.png" alt="Music Note Icon" />
-          </div>
-          <div class="icon animate__animated animate__bounceInLeft">
-            <img src="img/shuffle.png" alt="Shuffle Icon" />
-          </div>
-          <div class="icon animate__animated animate__bounceInLeft">
-            <img src="img/glasses.png" alt="Glasses Icon" />
-          </div>
-          <div class="icon animate__animated animate__bounceInLeft">
-            <img src="img/settings-1.png" alt="Settings Icon" />
-          </div>
-          <div class="icon animate__animated animate__bounceInLeft">
-            <img src="img/cog.png" alt="Cog Icon" />
-          </div>
-          <div class="icon animate__animated animate__bounceInLeft">
-            <img src="img/vector.png" alt="Vector Icon" />
-          </div>
-          <div class="icon animate__animated animate__bounceInLeft">
-            <img src="img/edit.png" alt="Edit Icon" />
-          </div>
-          <div class="icon animate__animated animate__bounceInLeft">
-            <img src="img/paper-plane.png" alt="Paper Plane Icon" />
-          </div>
-          <div class="icon animate__animated animate__bounceInLeft">
-            <img src="img/hand.png" alt="Hand Pointer Icon" />
-          </div>
-          <div class="icon animate__animated animate__bounceInLeft">
-            <img src="img/star.png" alt="Star Icon" />
-          </div>
-          <div class="icon animate__animated animate__bounceInLeft">
-            <img src="img/like.png" alt="Thumbs Up Icon" />
-          </div>
+           <img src="img/book.png" alt="Book Icon" />
+         </div>
+         <div class="icon animate__animated animate__bounceInLeft">
+           <img src="img/chat.png" alt="Chat Icon" w/>
+         </div>
+         <div class="icon animate__animated animate__bounceInLeft">
+           <img src="img/headphones.png" alt="Headphones Icon" />
+         </div>
+         <div class="icon animate__animated animate__bounceInLeft">
+           <img src="img/magnifying-glass.png" alt="Magnifying Glass Icon" />
+         </div>
+         <div class="icon animate__animated animate__bounceInLeft">
+           <img src="img/envelope.png" alt="Envelope Icon" />
+         </div>
+         <div class="icon animate__animated animate__bounceInLeft">
+           <img src="img/share.png" alt="Share Icon" />
+         </div>
+         <div class="icon animate__animated animate__bounceInLeft">
+           <img src="img/speaker.png" alt="Speaker Icon" />
+         </div>
+         <div class="icon animate__animated animate__bounceInLeft">
+           <img src="img/upload.png" alt="Upload Icon" />
+         </div>
+         <div class="icon animate__animated animate__bounceInLeft">
+           <img src="img/padlock.png" alt="Padlock Icon" />
+         </div>
+         <div class="icon animate__animated animate__bounceInLeft">
+           <img src="img/back.png" alt="Back Icon" />
+         </div>
+         <div class="icon animate__animated animate__bounceInLeft">
+           <img src="img/next-1.png" alt="Next Icon" />
+         </div>
+         <div class="icon animate__animated animate__bounceInLeft">
+           <img src="img/monitor.png" alt="Monitor Icon" />
+         </div>
+         <div class="icon animate__animated animate__bounceInLeft">
+           <img src="img/reload.png" alt="Reload Icon" />
+         </div>
+         <div class="icon animate__animated animate__bounceInLeft">
+           <img src="img/musical-note.png" alt="Music Note Icon" />
+         </div>
+         <div class="icon animate__animated animate__bounceInLeft">
+           <img src="img/shuffle.png" alt="Shuffle Icon" />
+         </div>
+         <div class="icon animate__animated animate__bounceInLeft">
+           <img src="img/glasses.png" alt="Glasses Icon" />
+         </div>
+         <div class="icon animate__animated animate__bounceInLeft">
+           <img src="img/settings-1.png" alt="Settings Icon" />
+         </div>
+         <div class="icon animate__animated animate__bounceInLeft">
+           <img src="img/cog.png" alt="Cog Icon" />
+         </div>
+         <div class="icon animate__animated animate__bounceInLeft">
+           <img src="img/vector.png" alt="Vector Icon" />
+         </div>
+         <div class="icon animate__animated animate__bounceInLeft">
+           <img src="img/edit.png" alt="Edit Icon" />
+         </div>
+         <div class="icon animate__animated animate__bounceInLeft">
+           <img src="img/paper-plane.png" alt="Paper Plane Icon" />
+         </div>
+         <div class="icon animate__animated animate__bounceInLeft">
+           <img src="img/hand.png" alt="Hand Pointer Icon" />
+         </div>
+         <div class="icon animate__animated animate__bounceInLeft">
+           <img src="img/star.png" alt="Star Icon" />
+         </div>
+         <div class="icon animate__animated animate__bounceInLeft">
+           <img src="img/like.png" alt="Thumbs Up Icon" />
+         </div>
         </div>
         <span class="name">Dylan Dills</span>
         <span class="job-title">Creative Developer</span>
         <div class="arrow bounce" id="down-arrow"></div>
       </section>
       <section id="about">
+        <h1 class="about-title">About</h1>
+        <img src="img/profile-photo.jpg" alt="Image of Dylan Dills standing" />
+        <p>Keytar +1 man braid, raclette before they sold out lumbersexual fixie jianbing health goth sustainable kale chips bushwick humblebrag cliche. PBR&B waistcoat twee YOLO lyft franzen. Selfies bespoke salvia deep v vice, organic taiyaki shabby chic letterpress crucifix pinterest blue bottle artisan. Viral YOLO vexillologist thundercats craft beer vinyl plaid palo santo messenger bag ugh gentrify deep v lomo succulents. Synth copper mug helvetica pitchfork gentrify roof party chartreuse fanny pack. Sustainable fingerstache la croix pabst ennui.</p>
       </section>
       <section id="projects">
       </section>
